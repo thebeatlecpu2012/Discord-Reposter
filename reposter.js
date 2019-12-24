7708a4e05e6757f76c8e7db84c61108a2701813e
thebeatlecpu2012 <57822156+thebeatlecpu2012@users.noreply.github.com>
2019-12-24 16:34:16 -0600

    Update reposter.js

--- reposter.js
+++ reposter.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("<SECRET_BOT_TOKEN>").catch(console.error);
+client.login("<dXyTpnyHFumPsSXcvysI_dvOKVB5CGeI>").catch(console.error);
 
 client.on("ready", function() {
 	client.user.setActivity(client.guilds.size + " server" + (client.guilds.size === 1 ? "" : "s"), { type: "WATCHING" }).catch(console.error);
